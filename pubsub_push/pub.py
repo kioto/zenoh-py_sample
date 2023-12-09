5c7e0b2080def2ab3bb9b3df40c6ec69404f7a99
Kioto Hirahara <kioto.hirahara@gmail.com>
2023-12-09 18:28:32 +0900

    Add host configuration

--- pubsub_push/pub.py
+++ pubsub_push/pub.py
@@ -1,4 +1,5 @@
 import time
+import json
 import zenoh
 
 
@@ -6,8 +7,15 @@ KEY_BASE = 'demo/example/'
 KEY_PUB_1 = KEY_BASE + 'pub1'
 KEY_PUB_2 = KEY_BASE + 'pub2'
 KEY_PUB_ALL = KEY_BASE + '**'
+HOST_1 = '127.0.0.1'
+HOST_2 = '127.0.0.1'
+
+ENDPOINT_1 = f'tcp/{HOST_1}'
+ENDPOINT_2 = f'tcp/{HOST_2}'
 
 CONF = zenoh.Config()
+CONF.insert_json5(zenoh.config.CONNECT_KEY,
+                  json.dumps([ENDPOINT_1, ENDPOINT_2]))
 
 
 if __name__ == '__main__':
@@ -21,6 +29,7 @@ if __name__ == '__main__':
     pub_all = session.declare_publisher(KEY_PUB_ALL)
 
     for idx in range(10):
+        print(f'publish {idx}')
         st = idx % 3
         if st == 1:
             pub1.put(f'message to pub1 {idx}')
@@ -31,6 +40,7 @@ if __name__ == '__main__':
 
         time.sleep(1)
 
+    print('done')
     pub_all.put('done')
 
     pub1.undeclare()
